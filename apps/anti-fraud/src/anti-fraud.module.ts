4a9120684cdcb5eb70cf4d85c3b4e1e3f96126e6
Edwin <equispecari.26@gmail.com>
2022-09-21 16:48:25 -0500

    add[anti-fraud]: Conect Kafka

--- apps/anti-fraud/src/anti-fraud.module.ts
+++ apps/anti-fraud/src/anti-fraud.module.ts
@@ -1,9 +1,22 @@
+import { KafkaModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AntiFraudController } from './anti-fraud.controller';
 import { AntiFraudService } from './anti-fraud.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        KAFKA_URI: Joi.string().required(),
+        KAFKA_ANTI_FRAUD_QUEUE: Joi.string().required(),
+      }),
+      envFilePath: './apps/anti-fraud/.env',
+    }),
+    KafkaModule,
+  ],
   controllers: [AntiFraudController],
   providers: [AntiFraudService],
 })
--- apps/anti-fraud/src/main.ts
+++ apps/anti-fraud/src/main.ts
@@ -1,8 +1,11 @@
+import { KafkaService } from '@app/common';
 import { NestFactory } from '@nestjs/core';
 import { AntiFraudModule } from './anti-fraud.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AntiFraudModule);
-  await app.listen(3000);
+  const kafkaService = app.get<KafkaService>(KafkaService);
+  app.connectMicroservice(kafkaService.getOptions('TRANSACTION'));
+  await app.startAllMicroservices();
 }
 bootstrap();
