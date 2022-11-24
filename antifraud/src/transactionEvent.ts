784291fb695c0f9083b48937e01e7fb9c3f25849
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-11-23 19:36:37 -0500

    antifraud test deleted

--- antifraud/src/transactionEvent.ts
+++
@@ -1,8 +0,0 @@
-export class TransactionEvent {
-    constructor(
-      public readonly id: number,
-      public readonly transactionExternalId: string,
-      public readonly transactionStatus: string,
-      public readonly value: number,
-    ) {}
-  }
