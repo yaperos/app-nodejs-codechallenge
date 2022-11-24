6447c0b3ea417d8ae810cd0c9164fd09368ed63d
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-11-23 19:27:38 -0500

    Feat(OldVersion): migration with oldVersion deleted

--- transaction/prisma/migrations/20221112195452_migrate/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `UpdateAt` on the `Transaction` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Transaction" DROP COLUMN "UpdateAt",
-ADD COLUMN     "updateAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
