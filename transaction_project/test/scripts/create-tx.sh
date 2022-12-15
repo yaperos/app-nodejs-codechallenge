0c1dd35c4e063882420be4f00a0498d06889fb48
mako mc <makomc751939>
2022-12-15 05:41:59 -0500

    fixed port in curl script

--- transaction_project/test/scripts/create-tx.sh
+++ transaction_project/test/scripts/create-tx.sh
@@ -1,1 +1,1 @@
-curl -d '{ "value": 100 }'  -H "Content-Type: application/json" -X POST http://localhost:4000/api/transaction
+curl -d '{ "value": 100 }'  -H "Content-Type: application/json" -X POST http://localhost:3000/api/transaction
