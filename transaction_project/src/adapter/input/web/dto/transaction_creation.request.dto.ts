0d1508ccad11643ad1eadfbd7921a55a7df6d693
mako mc <makomc751939>
2022-12-17 18:17:24 -0500

    validating an enum

--- transaction_project/src/adapter/input/web/dto/transaction_creation.request.dto.ts
+++ transaction_project/src/adapter/input/web/dto/transaction_creation.request.dto.ts
@@ -1,6 +1,9 @@
-import { IsString, IsUUID } from 'class-validator';
+import { IsIn, IsNumber, IsPositive, IsString, IsUUID } from 'class-validator';
+import { TransactionTypeEnum } from 'src/domain/models/transaction_type_enum';
 
 export class TransactionCreationRequestDto {
+  @IsPositive()
+  @IsNumber()
   value: number;
 
   @IsUUID()
@@ -11,5 +14,13 @@ export class TransactionCreationRequestDto {
   @IsString()
   accountExternalIdCredit: string;
 
+  // Custom validator for this enum
+  @IsIn(
+    Object.values(TransactionTypeEnum)
+      .filter((item) => {
+        return isNaN(Number(item));
+      })
+      .map((key) => TransactionTypeEnum[key]),
+  )
   transferTypeId: number;
 }
