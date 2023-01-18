b5b291c9adb14bd763479abe872fd74b783d355b
Christian Valencia <cros410@gmail.com>
2023-01-18 03:59:43 -0500

    Replace Generate uuid method
    Signed-off-by: Christian Valencia <cros410@gmail.com>

--- k6.js
+++ k6.js
@@ -1,30 +1,33 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import http from 'k6/http'
+import { sleep } from 'k6'
+import * as crypto from 'k6/crypto'
+
+const uuidv4 = () => crypto.md5(crypto.randomBytes(42), 'hex')
 
 export const options = {
   vus: 10,
   duration: '30s',
-};
+}
+
+
 
 export default function () {
-
-  const url = 'http://127.0.0.1:3000/transactions';
+  const url = 'http://127.0.0.1:3000/transactions'
   const payload = JSON.stringify({
     "accountExternalIdDebit": uuidv4(),
     "accountExternalIdCredit": uuidv4(),
     "tranferTypeId": 6,
     "value": 1
-  });
+  })
 
   const params = {
     headers: {
       'Content-Type': 'application/json',
     },
-  };
+  }
 
-  http.post(url, payload, params);
-  http.get(url);
+  http.post(url, payload, params)
+  http.get(url)
 
-  sleep(1);
+  sleep(1)
 }
