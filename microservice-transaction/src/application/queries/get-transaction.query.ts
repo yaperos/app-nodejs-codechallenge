9aca564ddc687f5d8dee2cca13da942e6958bed5
rpopi-ext <rpopi@caleidos.pe>
2023-01-26 21:46:22 -0500

    :sparkles: updated status transaction

---
+++ microservice-transaction/src/application/queries/get-transaction.query.ts
@@ -0,0 +1,21 @@
+import { Inject } from "@nestjs/common";
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { TransactionRepository } from "src/domain/repositories/transaction.repository";
+import { TransactionInfrastructure } from "src/infrastructure/transaction.infrastructure";
+import { GetTransactionByIdDto } from "../dtos/get-transaction-by-id.response.dto";
+
+export class GetTransactionQuery {
+  constructor(public readonly transactionExternalId: string) {}
+}
+
+@QueryHandler(GetTransactionQuery)
+export class GetTransactionQueryHandler implements IQueryHandler<GetTransactionQuery,GetTransactionByIdDto> {
+  constructor(
+     @Inject(TransactionInfrastructure) private transactionRepository: TransactionRepository
+  ) {}
+  async execute(query: GetTransactionQuery): Promise<GetTransactionByIdDto> {
+     const transaction = await this.transactionRepository.getTransactionById(query.transactionExternalId);
+     console.log('transaction', transaction);
+     return GetTransactionByIdDto.reponse(transaction);
+  }
+}
