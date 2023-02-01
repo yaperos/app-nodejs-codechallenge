776135f7cd2edcc9f12e93a11c28256bcfc5f24e
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2023-01-31 21:32:54 -0500

    refactor(database): Add logging

--- transaction-service/src/infraestructure/database/database.providers.ts
+++ transaction-service/src/infraestructure/database/database.providers.ts
@@ -1,6 +1,6 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService, ConfigModule } from '@nestjs/config';
-import { DataSource } from 'typeorm';
+import { DataSource, LoggerOptions } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export const databaseProviders = [
@@ -9,18 +9,20 @@ export const databaseProviders = [
     inject: [ConfigService],
     // Use useFactory, useClass, or useExisting
     // to configure the DataSourceOptions.
-    useFactory: (configService: ConfigService) => ({
-      type: 'postgres',
-      host: configService.get<string>('DATABASE_HOST'),
-      port: configService.get<number>('DATABASE_PORT'),
-      username: configService.get<string>('DATABASE_USERNAME'),
-      password: configService.get<string>('DATABASE_PASSWORD'),
-      database: configService.get<string>('DATABASE_NAME'),
-      entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-      namingStrategy: new SnakeNamingStrategy(),
-      synchronize: true,
-      reuseConnection: true,
-    }),
+    useFactory: (configService: ConfigService) =>
+      ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        logging: configService.get<LoggerOptions>('DB_LOG_LEVEL'),
+        entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+        namingStrategy: new SnakeNamingStrategy(),
+        synchronize: true,
+        reuseConnection: true,
+      } as TypeOrmModuleOptions),
     // dataSource receives the configured DataSourceOptions
     // and returns a Promise<DataSource>.
     dataSourceFactory: async (options) => {
