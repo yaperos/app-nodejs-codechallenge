a556f803e0e7d743b9757518ca8e8219500f4f7a
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2023-02-02 00:54:43 -0500

    refactor(transaction service): Add kafka and update bootstrap method

--- transaction-service/src/main.ts
+++ transaction-service/src/main.ts
@@ -1,10 +1,36 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions } from '@nestjs/microservices/interfaces';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(configService.get<string>('APP_PORT')).then(() => {
+    Logger.log(
+      `Service is listening on port ${configService.get<string>('APP_PORT')}`,
+    );
+  });
+
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'transaction',
+          brokers: [configService.get<string>('KAFKA_BROKER')],
+        },
+        consumer: {
+          groupId: 'transaction-consumer',
+        },
+      },
+    });
+
+  await microservice.listen().then(() => {
+    Logger.log(`Microservice is listening...`);
+  });
 }
 bootstrap();
