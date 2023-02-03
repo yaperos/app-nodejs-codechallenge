d3f881bc9595053b6710b8e7a5cd354fd89d9545
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2023-02-03 00:13:33 -0500

    fix(anti-fraud service): Delete reference to event service

--- anti-fraud-service/src/app.controller.ts
+++ anti-fraud-service/src/app.controller.ts
@@ -1,21 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
-import { json } from 'stream/consumers';
 import { ValidateService } from './validate.service';
-import { EventService } from './event.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly validateService: ValidateService,
-    //private readonly eventService: EventService,
-  ) {}
+  constructor(private readonly validateService: ValidateService) {}
 
   @EventPattern('transaction')
   validateTransaction(message: string) {
     console.log(`Received event: ${message}`);
     console.log(message);
-    //this.eventService.sendEvent(name);
   }
 
 }
