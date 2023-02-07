9bfc6173da88d838dda3d5527f7a654eb5e4f67d
Oscar Diaz <oscargdi@hotmail.com>
2023-02-07 04:19:41 +0000

    chore: load environment variables

--- apps/transaction/src/app.module.ts
+++ apps/transaction/src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/transaction/src/app.service.ts
+++ apps/transaction/src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello Transaction!';
+    return this.configService.get('DATABASE_URL');
   }
 }
