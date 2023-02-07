ff73cf8c06f0e169ed0028243f68092851377481
Oscar Diaz <oscargdi@hotmail.com>
2023-02-07 21:43:28 +0000

    chore: remove transaction module

--- apps/transaction/src/app.module.ts
+++ apps/transaction/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       ignoreEnvFile: true,
     }),
-    TransactionModule,
   ],
   controllers: [],
   providers: [],
