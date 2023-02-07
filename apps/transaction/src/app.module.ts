fb8b42c5a61e6a682f1b989f33ebe153a7d27de9
Oscar Diaz <oscargdi@hotmail.com>
2023-02-07 14:51:14 +0000

    chore: apply organize imports

--- apps/transaction/src/app.module.ts
+++ apps/transaction/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
