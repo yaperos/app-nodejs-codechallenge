98aa78375d75840f9752b7ca02fe81c79136d1d9
Oscar Diaz <oscargdi@hotmail.com>
2023-02-07 21:59:04 +0000

    chore: add transaction resource

--- apps/transaction/src/app.module.ts
+++ apps/transaction/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       ignoreEnvFile: true,
     }),
+    TransactionModule,
   ],
   controllers: [],
   providers: [],
