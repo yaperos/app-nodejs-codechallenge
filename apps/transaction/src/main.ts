5a843e862013f63091077be09cbc469fbecf38ed
Oscar Diaz <oscargdi@hotmail.com>
2023-02-08 16:23:48 +0000

    docs: add swagger api documentation

--- apps/transaction/src/main.ts
+++ apps/transaction/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Transaction API')
+    .setDescription(
+      'This API supports the creation and retrieve of transactions.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
