9113eff95a94efaf583197484ea8ed53e493cca6
Hans Felix <hansfelix50@gmail.com>
2023-02-23 11:05:25 -0500

    Add Prisma module

--- yape_transaction_service/src/app.module.ts
+++ yape_transaction_service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
