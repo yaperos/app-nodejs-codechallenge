74f782d129eadf698572e36509462054befc6265
Enzo Israel <neyra.dianderas@gmail.com>
2023-04-07 21:22:59 -0500

    fixin docker compose

--- anti-fraud-service/src/index.ts
+++ anti-fraud-service/src/index.ts
@@ -1,16 +1,17 @@
-import { KafkaService } from "common-microservice-lib";
+import { KafkaService } from 'common-microservice-lib';
 
 const setting = {
-  brokers: ["kafka:9092"],
-  clientId: "client-id-1",
-  groupId: "group-id-1",
+  brokers: ['kafka:9092'],
+  clientId: 'client-id-2',
+  groupId: 'group-id-1',
 };
 
-const transactionExternalTopic = "transaction-external";
+const transactionExternalTopic = 'transaction-external';
 
 const kafkaService = new KafkaService(setting);
 
 kafkaService.subcribeMessageFromTopic(transactionExternalTopic, (data) => {
+  console.log(':::onTransactionExternalCreatedEvent Received:::', data);
   kafkaService.sendMessageToTopic({
     topic: transactionExternalTopic,
     value: { id: data.transactionExternalId, isValid: data.value <= 1000 },
