be58d58ac879fef5becc95ea680288cf30b9dc67
Jorge Vásquez <jorge.vasquez@utec.edu.pe>
2023-04-23 18:36:09 -0500

    feat: redis module .env conf

--- apps/transaction-server/src/redis/redis.module.ts
+++ apps/transaction-server/src/redis/redis.module.ts
@@ -1,17 +1,19 @@
 import { Global, Module } from '@nestjs/common';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { redisStore } from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
     imports: [
+      ConfigModule.forRoot(),
         CacheModule.register({
             // @ts-ignore
             store: async () => await redisStore({
               // Store-specific configuration:
               socket: {
-                host: 'localhost',
-                port: 6379,
+                host: process.env.REDIS_HOST,
+                port: Number(process.env.REDIS_PORT),
               }
             })
           }),
