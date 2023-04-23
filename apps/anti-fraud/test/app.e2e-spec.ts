a1eb8e23dc70976c6a31b9350fe6186ce846c237
Jorge Vásquez <jorge.vasquez@utec.edu.pe>
2023-04-23 15:22:40 -0500

    fix: rootdir

--- apps/anti-fraud/test/app.e2e-spec.ts
+++ apps/anti-fraud/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AntiFraudModule } from './../src/anti-fraud.module';
+import { AntiFraudModule } from '../src/anti-fraud.module';
 
 describe('AntiFraudController (e2e)', () => {
   let app: INestApplication;
