36f929813303909c05caa6227570db9b6bf3096f
Jorge Vásquez <jorge.vasquez@utec.edu.pe>
2023-04-23 22:16:57 -0500

    fix: anti-fraud service

--- apps/transaction-server/src/anti-fraud-receiver/anti-fraud-receiver.service.ts
+++ apps/transaction-server/src/anti-fraud-receiver/anti-fraud-receiver.service.ts
@@ -5,66 +5,46 @@ import { Cache as CacheManager } from 'cache-manager';
 @Injectable()
 export class AntiFraudReceiverService {
 
-        constructor(
-            private prisma: PrismaService,
-            @Inject(CACHE_MANAGER) private cacheManager: CacheManager,
-    
-        ) { }
+    constructor(
+        private prisma: PrismaService,
+        @Inject(CACHE_MANAGER) private cacheManager: CacheManager,
+
+    ) { }
+
+
+    private async updateStatus(data: any, statusId: number) {
+        const { id } = data;
+        console.log('transactionId: ', id);
+        let obj = await this.prisma.yapeTransaction.update({
+            select: {
+                transactionExternalId: true,
+                value: true,
+                createdAt: true,
+                transactionType: {
+                    select: {
+                        name: true
+                    }
+                },
+                transactionStatus: {
+                    select: {
+                        name: true
+                    }
+                }
+            },
+            where: { transactionExternalId: id },
+            data: { transactionStatusId: statusId },
+        });
+        await this.cacheManager.set(obj.transactionExternalId, obj);
+        console.log('obj: ', obj);
+    }
 
     async handleAntiSuccess(data: any) {
-            console.log('AntiFraudReceiverService.handleAntiSuccess');
-            const { id } = data;
-            console.log('transactionId: ', id);
-            let obj = await this.prisma.yapeTransaction.update({
-                select: {
-                    transactionExternalId: true,
-                    value: true,
-                    createdAt: true,
-                    transactionType: {
-                        select: {
-                            name: true
-                        }
-                    },
-                    transactionStatus: {
-                        select: {
-                            name: true
-                        }
-                    }
-                },
-                where: { transactionExternalId: id },
-                data: { transactionStatusId: 2 },
-            });
-            await this.cacheManager.set(obj.transactionExternalId, obj);
-            console.log('obj: ', obj);
-
-        }
+        this.updateStatus(data, 2);
+    }
 
     async handleAntiWrong(data: any) {
-            console.log('AntiFraudReceiverService.handleAntiWrong');
-            const { id } = data;
-            console.log('transactionId: ', id);
-            let obj = await this.prisma.yapeTransaction.update({
-                select: {
-                    transactionExternalId: true,
-                    value: true,
-                    createdAt: true,
-                    transactionType: {
-                        select: {
-                            name: true
-                        }
-                    },
-                    transactionStatus: {
-                        select: {
-                            name: true
-                        }
-                    }
-                },
-                where: { transactionExternalId: id },
-                data: { transactionStatusId: 3 },
-            });
-            await this.cacheManager.set(obj.transactionExternalId, obj);
-            console.log('obj: ', obj);
-        }
-
+        this.updateStatus(data, 3);
     }
+
+}
 
