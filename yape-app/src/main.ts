1ed98c510c5c12e3a0338a5d35b40799352a03f3
Franz Maguina <fmaguinaa@gmail.com>
2023-05-12 21:46:08 -0500

    adding swagger

--- yape-app/src/main.ts
+++ yape-app/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Yape transactions service')
+    .setDescription('yape transacition api documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- yape-app/src/transaction/transaction.controller.ts
+++ yape-app/src/transaction/transaction.controller.ts
@@ -1,28 +1,33 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { SaveTransactionRequest, UpdateTransactionRequest } from './dto';
+import { Transaction } from 'src/entities';
 
 @Controller('transaction')
 export class TransactionController {
   constructor(private transactionService: TransactionService) {}
 
   @Get()
-  get() {
+  get(): Promise<Transaction[]> {
     return this.transactionService.getTransactions();
   }
 
   @Get('/:id')
-  getId(@Param('id') id: string) {
+  getId(@Param('id') id: string): Promise<Transaction[]> {
     return this.transactionService.getTransaction(id);
   }
 
   @Post('/save')
-  save(@Body() saveTransactionRequest: SaveTransactionRequest) {
+  save(
+    @Body() saveTransactionRequest: SaveTransactionRequest,
+  ): Promise<Transaction> {
     return this.transactionService.save(saveTransactionRequest);
   }
 
   @Post('/update')
-  update(@Body() updateTransactionRequest: UpdateTransactionRequest) {
+  update(
+    @Body() updateTransactionRequest: UpdateTransactionRequest,
+  ): Promise<Transaction> {
     return this.transactionService.update(updateTransactionRequest);
   }
 }
