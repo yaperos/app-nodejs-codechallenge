90d6847e131a37ac80fb5d4f5a6ec6fd8bf64cca
RodLopezDev <rodrigolopezdev@gmail.com>
2023-07-13 12:28:53 -0500

    chore: :wrench: cleanup on code

--- nest-transaction/src/app.module.ts
+++ nest-transaction/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
 import { CustomMongooseModule } from './app/mongo';
 import { getEnvironmentVars } from './app/Enviroment';
 import { TransactionModule } from './modules/transaction/transaction.module';
@@ -16,7 +14,7 @@ import { TransactionModule } from './modules/transaction/transaction.module';
     CustomMongooseModule,
     TransactionModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- nest-transaction/src/modules/transaction/transaction.controller.ts
+++ nest-transaction/src/modules/transaction/transaction.controller.ts
@@ -1,10 +1,11 @@
+import { ApiTags } from '@nestjs/swagger';
 import { Get, Post, Body, Param, Delete, Controller } from '@nestjs/common';
 
 import { TransactionService } from './transaction.service';
+import { ParseMongoIdPipe } from '../../common/parse-mongo-id.pipe';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('transaction')
+@ApiTags('Transaction')
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
@@ -20,12 +21,12 @@ export class TransactionController {
   }
 
   @Get(':transactionId')
-  findOne(@Param('transactionId') transactionId: string) {
+  findOne(@Param('transactionId', ParseMongoIdPipe) transactionId: string) {
     return this.transactionService.findOne(transactionId);
   }
 
   @Delete(':transactionId')
-  remove(@Param('transactionId') transactionId: string) {
+  remove(@Param('transactionId', ParseMongoIdPipe) transactionId: string) {
     return this.transactionService.remove(transactionId);
   }
 }
