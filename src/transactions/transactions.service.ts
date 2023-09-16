60e50cfce69e71622ef9e4cb44ab7dfd2dbf2687
Jinme Mirabal <mirabalj@gmail.com>
2023-09-16 10:48:40 -0500

    Validating columns in method to create transactions

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -8,4 +8,14 @@ export const findTransaction = async(id: string): Promise<Transaction> => modelT
 
 export const findType = async(id: number): Promise<TypeTransaction> => modelTransaction.getType(id);
 
-export const createTransaction = async(transaction: BaseTransaction): Promise<Transaction> => modelTransaction.createTransaction(transaction);
+export const createTransaction = async(transaction: BaseTransaction): Promise<Transaction> => {
+  // Validating value column
+  const { tranferTypeId = 0, value = 0 } = transaction;
+  if (value > 1000 || value <= 0) throw new Error("Value must be between 1 and 1000");
+
+  // Validating if transaction type exists
+  const type = await findType(tranferTypeId);
+  if (!type) throw new Error("Transaction type doesn't exists");
+
+  return modelTransaction.createTransaction(transaction);
+}
