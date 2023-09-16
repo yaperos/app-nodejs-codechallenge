6b14d290fe7dda3d77d80d3c57853d8ff42088e5
Jinme Mirabal <mirabalj@gmail.com>
2023-09-16 09:54:29 -0500

    Added routes to Express server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import 'dotenv/config';
+import { transactionsRouter } from './transactions/transactions.controller';
 
 require('dotenv').config();
 
@@ -14,6 +15,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', transactionsRouter);
 
 const start = async () => {
   try {
