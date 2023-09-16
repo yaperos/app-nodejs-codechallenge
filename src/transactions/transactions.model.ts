71dd574f3eac66091de450398ccf9b67eae422d5
Jinme Mirabal <mirabalj@gmail.com>
2023-09-16 09:31:45 -0500

    Created model to access database with Prisma

---
+++ src/transactions/transactions.model.ts
@@ -0,0 +1,21 @@
+import { BaseTransaction, Transaction, TypeTransaction } from "./transactions.interface";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+class Transactions {
+  async getTransaction(id: string): Promise<Transaction> {
+    const [ transaction ] = await prisma.transaction.findMany({ where: { id }});
+    return transaction;
+  };
+  async getType(id: number): Promise<TypeTransaction> {
+    const [ type ] = await prisma.transactionType.findMany({ where: { id }});
+    return type;
+  };
+  async createTransaction(transaction: any): Promise<Transaction> {
+    const created = await prisma.transaction.create(transaction)
+    return created;
+  }
+};
+
+export default new Transactions();
