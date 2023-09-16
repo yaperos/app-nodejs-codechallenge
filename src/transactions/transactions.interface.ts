b121647447e8bd9ea3c99d4a11ca4593b9e609ec
Jinme Mirabal <mirabalj@gmail.com>
2023-09-16 08:51:49 -0500

    Added interface to manage transactions module

---
+++ src/transactions/transactions.interface.ts
@@ -0,0 +1,11 @@
+export interface BaseTransaction {
+  accountExternalIdDebit: string;
+  accountExternalIdCredit: String;
+  tranferTypeId: number;
+  value: number;
+  createdAt: Date;
+}
+
+export interface Transaction extends BaseTransaction {
+  id: string;
+}
