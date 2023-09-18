b0c39ba808df246c8858c17969cc69de708a4cd9
Jinme Mirabal <mirabalj@gmail.com>
2023-09-17 22:15:10 -0500

    Added fake consumer to simulate anti-fraud service

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,19 @@ import 'dotenv/config';
 import { transactionsRouter } from './transactions/transactions.controller';
 import { errorHandler } from './middleware/error.middleware';
 import { notFoundHandler } from './middleware/not-found.middleware';
+import { antiFraudService } from './common/anti-fraud.service';
+import Kafka from './common/kafka.provider';
 
 require('dotenv').config();
 
 if (!process.env.PORT) process.exit(1);
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
+const KAFKA_TOPIC = process.env.KAFKA_TOPIC || 'kafka-topic';
+const KAFKA_GROUP_ID = process.env.KAFKA_GROUP_ID || 'group-id'
+
+const kafkaClient = Kafka.getClient();
+const consumer = kafkaClient.consumer({ groupId: KAFKA_GROUP_ID });
 
 const app = express();
 
@@ -25,6 +32,14 @@ app.use(notFoundHandler);
 
 const start = async () => {
   try {
+    // Running a Kafka antiFraud consumer
+    await consumer.connect();
+    await consumer.subscribe({ topic: KAFKA_TOPIC, fromBeginning: true });
+    await consumer.run({
+      eachMessage: antiFraudService,
+    });
+
+    // Starting Express server
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
