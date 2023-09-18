1410c60f23997f3dcf567f785470ad76469aa26a
Jinme Mirabal <mirabalj@gmail.com>
2023-09-18 08:09:20 -0500

    Adding response to anti-fraud simulated service

--- src/common/anti-fraud.service.ts
+++ src/common/anti-fraud.service.ts
@@ -1,19 +1,34 @@
 import { EachMessagePayload, ProducerRecord } from "kafkajs";
 import { Transaction } from "../transactions/transactions.interface";
 import Kafka from './kafka.provider';
+import fetchApi from "./fetch.api";
 
+const URL_CALLBACK = process.env.URL_CALLBACK || 'localhost';
 const KAFKA_TOPIC = process.env.KAFKA_TOPIC || 'kafka-topic';
 
 const kafkaClient = Kafka.getClient();
 const producer = kafkaClient.producer();
+
+const rejected = (max: number): boolean => (Math.floor(Math.random() * max) === 0);
 
 // Fake AntiFraud System
 export const antiFraudService = async(payload: EachMessagePayload): Promise<void> => {
   const { topic, partition, message } = payload;
   const value = message.value?.toString();
   if (value) {
-    const petition = JSON.parse(value);
-    console.log({ petition });
+    const { id } = JSON.parse(value);
+    const transactionStatus = {
+      id,
+      // Simulating 33% rejected
+      name: rejected(3) ? 'rejected' : 'approved',
+    };
+
+    // Returning response to client callback
+    try {
+      await fetchApi.post(URL_CALLBACK, transactionStatus);
+    } catch (e) {
+      console.log(e);
+    }
   };
 };
 
@@ -26,6 +41,6 @@ export const producerAntiFraud = async(transaction: Transaction) => {
       value: JSON.stringify(transaction),
     }]
   });
-  console.log('Request sending to anti-fraud service');
+  console.log('Request sent to anti-fraud service');
   await producer.disconnect();
-}
+};
