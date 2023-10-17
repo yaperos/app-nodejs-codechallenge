2dd0e20c6af133ffc91ffc625cb446f0462ccee4
Micky <miguel.zegarra@getcometa.com>
2023-10-16 22:14:51 -0500

    fix(test): adding env to testing

--- apps/antifraud/src/antifraud.utils.spec.ts
+++ apps/antifraud/src/antifraud.utils.spec.ts
@@ -3,11 +3,13 @@ import { getTransactionStatus } from './utils';
 describe('AntifraudController', () => {
   describe('root', () => {
     it('should return "rejected"', async () => {
+      process.env.TRANSACTION_REJECTED_STATUS = 'rejected';
       const result = await getTransactionStatus(29999999);
       expect(result).toBe('rejected');
     });
 
     it('should return "success"', async () => {
+      process.env.TRANSACTION_SUCCESS_STATUS = 'success';
       const result = await getTransactionStatus(999);
       expect(result).toBe('success');
     });
