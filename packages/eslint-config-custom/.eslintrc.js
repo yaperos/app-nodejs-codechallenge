4ef70049b4b172ff962027d8b788bf2d32e65a0f
Daniel Viloria <daniielvp82@gmail.com>
2023-10-30 23:01:35 -0500

    feature(): add project repo configurations

---
+++ packages/eslint-config-custom/.eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'perfectionist'],
+  rules: {
+    'perfectionist/sort-exports': [
+      'error',
+      {
+        order: 'asc',
+        type: 'natural',
+      },
+    ],
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        groups: [
+          'type',
+          ['builtin', 'external'],
+          'internal-type',
+          'internal',
+          ['parent-type', 'sibling-type', 'index-type'],
+          ['parent', 'sibling', 'index'],
+          'side-effect',
+          'style',
+          'object',
+          'unknown',
+        ],
+        order: 'asc',
+        type: 'natural',
+      },
+    ],
+    'perfectionist/sort-named-imports': [
+      'error',
+      {
+        order: 'asc',
+        type: 'natural',
+      },
+    ],
+    'perfectionist/sort-objects': [
+      'error',
+      {
+        order: 'asc',
+        type: 'natural',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
