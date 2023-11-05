a1e0f81c1f05cce7965e33489a7325a308a26f31
Stephen Cataño <>
2023-11-05 10:45:24 -0500

    Prepare Base Module, Controller and Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import expressConfig from '@config/express.config';
+import { TransactionsModule } from '@modules/transactions/transactions.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [expressConfig],
     }),
+    TransactionsModule,
   ],
 
   controllers: [AppController],
