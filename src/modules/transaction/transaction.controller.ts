a894bf8658d7464851a9111e332265df780e15c2
davidjesus1284 <davidjesus1284@gmail.com>
2023-11-05 20:57:33 -0500

    Added sending of data to kafka event when the transaction has been validated

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -12,6 +12,7 @@ import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dtos/create-transaction.dto';
 import { MessageKafkaPayloadDto } from './dtos/message-kafka.dto';
+import { ITransactionResponse } from './interfaces/transaction.interface';
 
 @Controller('transaction')
 export class TransactionController {
@@ -24,9 +25,9 @@ export class TransactionController {
   @Post('/')
   public async create(
     @Body() transactions: CreateTransactionDto[],
-  ): Promise<any> {
+  ): Promise<ITransactionResponse[]> {
     const transaction = await this.service.createTransanction(transactions);
-    this.kafka.emit('Anti-Fraud', JSON.stringify(transaction));
+    this.emitMessageTransaction(transaction);
     return transaction;
   }
 
@@ -44,11 +45,21 @@ export class TransactionController {
   public async messageTransaction(
     @Payload() payload: MessageKafkaPayloadDto[],
   ) {
-    Logger.log(
-      'Payload que regresa cuando se consume la cola',
-      JSON.stringify(payload),
-    );
     const response = await this.service.updateTransaction(payload);
-    Logger.log('message', JSON.stringify(response));
+    Logger.log('Verified transactions', JSON.stringify(response));
+    this.emitMessage(response);
+  }
+
+  @MessagePattern('Transaction')
+  public async transaction(@Payload() payload: MessageKafkaPayloadDto[]) {
+    Logger.log('Transaction finish process', JSON.stringify(payload));
+  }
+
+  private emitMessage(transaction: ITransactionResponse[]) {
+    this.kafka.emit('Transaction', JSON.stringify(transaction));
+  }
+
+  private emitMessageTransaction(transaction: ITransactionResponse[]) {
+    this.kafka.emit('Anti-Fraud', JSON.stringify(transaction));
   }
 }
