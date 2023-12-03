d5d5f7423931be53f9ca744b6588c1630059d6bb
Miguel Angel Ochoa Torres <mig8at@gmail.com>
2023-12-02 23:08:17 -0500

    add confirm

--- apps/antifraud/src/antifraud.service.ts
+++ apps/antifraud/src/antifraud.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AntifraudService {
-  constructor(@Inject('ANTIFRAUD') private readonly antifraud: any) {}
+  constructor(@Inject('ANTIFRAUD') private readonly antifraud: any) { }
 
   confirmed(id: string, value: number) {
     let status = 'pending';
@@ -11,6 +11,8 @@ export class AntifraudService {
     } else {
       status = 'rejected';
     }
+
+    console.log({ id, status });
     this.antifraud.emit('antifraud.confirmed', JSON.stringify({ id, status }));
   }
 }
--- apps/yape/src/transaction/transaction.controller.ts
+++ apps/yape/src/transaction/transaction.controller.ts
@@ -1,12 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { TransactionService } from './transaction.service';
 
 @Controller()
 export class TransactionController {
-    constructor() { }
+    constructor(private readonly service: TransactionService) { }
 
     @MessagePattern('antifraud.confirmed')
     confirmed(@Payload() { id, status }: any) {
         console.log({ id, status });
+        return this.service.confirmed(id, status);
     }
 }
