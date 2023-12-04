f93a89d837e8a397949fc9034925da6096de4622
kLizarraga <kewin392@gmail.com>
2023-12-04 00:05:35 -0500

    seed for DB

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO transaction_status (id, name) VALUES (1, 'pending');
+INSERT INTO transaction_status (id, name) VALUES (2, 'approved');
+INSERT INTO transaction_status (id, name) VALUES (3, 'rejected');
+
+INSERT INTO transaction_type (id, name) VALUES (1, 'commercial');
