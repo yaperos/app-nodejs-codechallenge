2e452362b00bdd1287eb2d51c22482d8faa18d97
Oscar Diaz <oscargdi@hotmail.com>
2024-01-22 03:33:43 +0000

    Add TypeORM naming strategy and update data source configuration

--- libs/database/src/data-source.ts
+++ libs/database/src/data-source.ts
@@ -1,13 +1,26 @@
 import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 config();
+console.log(__dirname);
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['dist/**/entities/*.js}'],
-  migrations: ['dist/**/migrations/*.js'],
+  synchronize: false,
+  logging: true,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: [
+    process.env.TYPEORM_CLI === 'true'
+      ? 'libs/database/src/entities/*.ts'
+      : 'dist/**/entities/*.js',
+  ],
+  migrations: [
+    process.env.TYPEORM_CLI === 'true'
+      ? 'libs/database/src/migrations/*.ts'
+      : 'dist/**/migrations/*.js',
+  ],
 };
 
 const dataSource = new DataSource(dataSourceOptions);
