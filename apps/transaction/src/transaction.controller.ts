477757d392c5ea28079a92de6bbfe404ec81c6b8
Oscar Diaz <oscargdi@hotmail.com>
2024-01-22 04:01:41 +0000

    Add GET endpoint to retrieve transaction by ID

--- apps/transaction/src/transaction.controller.ts
+++ apps/transaction/src/transaction.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { GetTransactionDto } from './dto/get-transaction.dto';
 import { TransactionService } from './transaction.service';
 
 @Controller('transaction')
@@ -12,8 +13,26 @@ export class TransactionController {
   @ApiResponse({
     status: 201,
     description: 'The transaction has been successfully created.',
+    type: GetTransactionDto,
   })
   async createTransaction(@Body() data: CreateTransactionDto) {
     return this.transactionService.createTransaction(data);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a transaction by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'The transaction has been successfully retrieved.',
+    type: GetTransactionDto,
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    format: 'uuid',
+    example: '60c06d21-9fd2-4de6-89b6-c196c66e9aa2',
+  })
+  async getTransactionById(id: string) {
+    return this.transactionService.getTransactionById(id);
   }
 }
--- apps/transaction/src/transaction.service.ts
+++ apps/transaction/src/transaction.service.ts
@@ -2,8 +2,9 @@ import { Transaction } from '@app/database/entities/transaction';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { GetTransactionDto } from './dto/get-transaction.dto';
 
 @Injectable()
 export class TransactionService {
@@ -18,6 +19,30 @@ export class TransactionService {
     transaction.accountExternalIdDebit = data.accountExternalIdDebit;
     transaction.accountExternalIdCredit = data.accountExternalIdCredit;
     transaction.value = data.value;
-    return this.transactionRepository.save(transaction);
+    const createdTransaction =
+      await this.transactionRepository.save(transaction);
+
+    const dto = new GetTransactionDto();
+    dto.id = createdTransaction.id;
+    dto.value = createdTransaction.value;
+    dto.status = createdTransaction.status;
+    dto.createdAt = createdTransaction.createdAt;
+    dto.updatedAt = createdTransaction.updatedAt;
+    return dto;
+  }
+
+  async getTransactionById(id: string): Promise<GetTransactionDto> {
+    const options: FindOptionsWhere<Transaction> = { id };
+    const transaction = await this.transactionRepository.findOneBy(options);
+    if (!transaction) {
+      throw new Error('Transaction not found');
+    }
+    const dto = new GetTransactionDto();
+    dto.id = transaction.id;
+    dto.value = transaction.value;
+    dto.status = transaction.status;
+    dto.createdAt = transaction.createdAt;
+    dto.updatedAt = transaction.updatedAt;
+    return dto;
   }
 }
