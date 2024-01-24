dc7c67e8080a5faeb9eb5046f6b8fa3de66d8e1d
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-24 17:49:59 -0500

    chore(common/yapeKafkaCore): add configuration for centralization of environment variables
    Added:
    - config/index.ts

---
+++ common/YapeKafkaCore/src/config/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+
+export const ConfigEnv = {
+  kafka: {
+    clientId: process.env.KAFKA_CLIENT_ID,
+    brokers: (process.env.KAFKA_BROKERS || "")
+      .split(",")
+      .map((broker) => broker.trim())
+      .filter(Boolean),
+  },
+  topics: {
+    transaction: process.env.KAFKA_TOPIC_TRANSACTION || "transaction-local",
+    transactionRequest:
+      process.env.KAFKA_TOPIC_TRANSACTION_REQUEST ||
+      "transaction-request-local",
+    transactionStatus:
+      process.env.KAFKA_TOPIC_TRANSACTION_STATUS || "transaction-status-local",
+  },
+};
