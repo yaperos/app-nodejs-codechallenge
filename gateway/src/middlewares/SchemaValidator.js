dc8ed3d1e4280645cea96482e7568cbe1c44f19f
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-24 20:02:16 -0500

    feat(gateway): Add middleware for schema validation
    Added:
    - src/middlewares/SchemaValidator.js

---
+++ gateway/src/middlewares/SchemaValidator.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+const createError = require('http-errors');
+
+module.exports.SchemaValidator = (schema, check = 'body') =>
+  (req, _res, next) => {
+    try {
+      if (Joi.isSchema(schema)) {
+        const { error, value } = schema.validate(req[check], { abortEarly: false })
+        if (error) {
+          throw createError(400, getJoiError(error).message)
+        } else {
+          req[check] = value
+          next()
+        }
+      } else throw createError(500, 'Schema is not a Joi schema')
+    } catch (error) {
+      next(error)
+    }
+  }
+
+const getJoiError = ({ details }) => {
+  const { message } = details[0]
+  return { message: message.replaceAll(' ', '_').replaceAll('"', '') }
+}
