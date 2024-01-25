8b1e1fdf65b28bec95db1e28b9e214900f9e3aa4
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-24 20:05:44 -0500

    feat(gateway): Add the transactions module to the gateway
    Updated:
    - src/index.js

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -6,6 +6,8 @@ const { NotFoundErrorHandler } = require("./middlewares/NotFoundErrorHandler");
 const { ErrorHandler } = require("./middlewares/ErrorHandler");
 const { ConfigEnv } = require("./config");
 
+const { TransactionRouter } = require("./modules/transactions");
+
 const app = express();
 
 app.use(helmet());
@@ -15,6 +17,8 @@ app.use(express.json());
 app.get("/helth-check", (_req, res, _next) => {
   res.status(200).json({ message: "Helth check ok" });
 });
+
+app.use("/v1/transactions", TransactionRouter);
 
 app.use(ErrorHandler);
 app.use(NotFoundErrorHandler);
