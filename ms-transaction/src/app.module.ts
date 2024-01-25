5213c195d6a3689fd04f97b90febdf26f03dc4c5
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-24 21:17:27 -0500

    chore(ms-transactions): Add initial transaction module configuration
    Added:
    - src/modules/transactions/transactions.module.ts
    
    Updated:
    - src/app.module.ts

--- ms-transaction/src/app.module.ts
+++ ms-transaction/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
   controllers: [AppController],
+  imports: [TransactionsModule],
 })
 export class AppModule {}
