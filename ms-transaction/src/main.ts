fb1d5d5329ee7945683c9dec4575887ba71d718a
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-24 21:07:14 -0500

    chore(ms-transaction): Service health check configuration.
    Updated:
    - src/app.controller.ts
    - src/app.module.ts
    - src/main.ts
    
    Deleted:
    - src/app.service.ts

--- ms-transaction/src/main.ts
+++ ms-transaction/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigEnv } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(ConfigEnv.port, () => {
+    console.log(`Transaction service is running on port ${ConfigEnv.port}`);
+  });
 }
 bootstrap();
