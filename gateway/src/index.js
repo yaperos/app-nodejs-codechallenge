4742f349e31cb53293e6e92ef0e4ab3cb0bf5f7f
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-24 19:15:25 -0500

    feat(gateway): Added initial server configuration and created middlewares for error management.
    Added:
    - src/index.js
    - src/middlewares/ErrorHandler.js
    - src/middlewares/NotFoundErrorHandler.js

---
+++ gateway/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const { NotFoundErrorHandler } = require("./middlewares/NotFoundErrorHandler");
+const { ErrorHandler } = require("./middlewares/ErrorHandler");
+const { ConfigEnv } = require("./config");
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan(":method :status :url :response-time ms"));
+app.use(express.json());
+
+app.get("/helth-check", (_req, res, _next) => {
+  res.status(200).json({ message: "Helth check ok" });
+});
+
+app.use(ErrorHandler);
+app.use(NotFoundErrorHandler);
+
+app.listen(ConfigEnv.port, () => {
+  console.log(`Server running on port ${ConfigEnv.port}`);
+});
---
+++ gateway/src/middlewares/NotFoundErrorHandler.js
@@ -0,0 +1,7 @@
+const createError = require("http-errors");
+
+const NOT_FOUND_CODE = 404;
+
+module.exports.NotFoundErrorHandler = (req, res) => {
+  res.status(NOT_FOUND_CODE).send(createError(NOT_FOUND_CODE));
+};
