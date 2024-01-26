251829a7e8c932642d2a8c29ce7c3abae73cbc9d
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-26 11:26:38 -0500

    feat(ms-error-report): Added new properties for tracking and traceability
    Added:
    - src/modules/database/models/ErrorReportSchema.js

--- ms-error-report/src/modules/database/models/ErrorReportSchema.js
+++ ms-error-report/src/modules/database/models/ErrorReportSchema.js
@@ -4,7 +4,9 @@ const { MongoDB } = require("../lib");
 
 const ErrorReportSchema = new Schema(
   {
-    transactionId: { type: SchemaTypes.String, required: true },
+    reportedBy: { type: SchemaTypes.String, required: true },
+    transactionId: { type: SchemaTypes.String },
+    correlationId: { type: SchemaTypes.String },
     name: { type: SchemaTypes.String },
     message: { type: SchemaTypes.String },
     stack: { type: Object },
