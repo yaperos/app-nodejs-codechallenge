9631a3dc115e78eb443e1ac90fcf45b09f8394a7
Geovanny G. Arguello Costta <giothcode@gmail.com>
2024-01-27 09:14:28 -0500

    feat(ms-error-report): Update error reporting schema
    Updated:
    - src/modules/database/models/ErrorReportSchema.js

--- ms-error-report/src/modules/database/models/ErrorReportSchema.js
+++ ms-error-report/src/modules/database/models/ErrorReportSchema.js
@@ -2,14 +2,28 @@ const { Schema, SchemaTypes } = require("mongoose");
 
 const { MongoDB } = require("../lib");
 
+const UnrecordedTransactionSchema = new Schema({
+  accountExternalIdDebit: { type: SchemaTypes.String },
+  accountExternalIdCredit: { type: SchemaTypes.String },
+  transferTypeId: { type: SchemaTypes.Number },
+  value: { type: SchemaTypes.Number },
+}, { _id: false })
+
+const ErrorSchema = new Schema({
+  name: { type: SchemaTypes.String },
+  message: { type: SchemaTypes.String },
+  stack: { type: SchemaTypes.Object },
+}, { _id: false });
+
 const ErrorReportSchema = new Schema(
   {
+    errorType: { type: SchemaTypes.String, required: true },
     reportedBy: { type: SchemaTypes.String, required: true },
     transactionId: { type: SchemaTypes.String },
     correlationId: { type: SchemaTypes.String },
-    name: { type: SchemaTypes.String },
-    message: { type: SchemaTypes.String },
-    stack: { type: Object },
+    attempts: { type: SchemaTypes.Number },
+    error: { type: ErrorSchema },
+    unrecordedTransaction: { type: UnrecordedTransactionSchema },
   },
   MongoDB.schemaOptions
 );
