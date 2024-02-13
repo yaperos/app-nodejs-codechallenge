8180c7ef3cf992df16f55c768d794e287e8410cd
Ezequiel Binker <ezebinker@gmail.com>
2024-02-12 21:05:07 -0300

    fix: remove exclude from transaction status id

--- apps/transaction-ms/src/entities/transaction-status.entity.ts
+++ apps/transaction-ms/src/entities/transaction-status.entity.ts
@@ -1,10 +1,8 @@
-import { Exclude } from 'class-transformer';
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class TransactionStatus {
   @PrimaryGeneratedColumn()
-  @Exclude()
   id: number;
 
   @Column({ length: 200 })
