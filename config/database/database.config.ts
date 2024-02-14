eaccb8f302f9e3e328d7303d6791f6495d9c1862
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-14 18:15:31 -0500

    :sparkles: ignore temporal migrations

--- config/database/database.config.ts
+++ config/database/database.config.ts
@@ -1,18 +1,18 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
-import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
+  type: "postgres",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '../../**/*.entity{.ts,.js}'],
-  migrations: ['dist/database/migrations/*.js'],
-  migrationsTableName: 'migrations',
-  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  entities: [__dirname + "../../**/*.entity{.ts,.js}"],
+  // migrations: [__dirname + "../migrations/*.ts"],
+  // migrationsTableName: "migrations",
+  synchronize: process.env.DB_SYNCHRONIZE === "true",
 };
 
 const dataSource = new DataSource(dataSourceOptions);
