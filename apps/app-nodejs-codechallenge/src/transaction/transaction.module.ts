9bd04ad71c304f1c97daedc8197a749cda753576
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-16 14:45:14 -0500

    :sparkles: create config for kafka as producer

--- apps/app-nodejs-codechallenge/src/transaction/transaction.module.ts
+++ apps/app-nodejs-codechallenge/src/transaction/transaction.module.ts
@@ -3,9 +3,14 @@ import TransactionService from "./transaction.service";
 import TransactionController from "./transaction.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import Transaction from "./entities/transaction.entity";
+import { ClientsModule } from "@nestjs/microservices";
+import kafkaProducerConfig from "../common/config/kafka.producer.config";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction]),
+    ClientsModule.register(kafkaProducerConfig),
+  ],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
