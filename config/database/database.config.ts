0b2d0c0bfb8bc7cbb9b5acf78d3d32b1c5f55178
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-15 19:40:03 -0500

    :sparkles: set path for search entities in apps

--- config/database/database.config.ts
+++ config/database/database.config.ts
@@ -5,14 +5,12 @@ dotenv.config();
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+  port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + "../../**/*.entity{.ts,.js}"],
-  // migrations: [__dirname + "../migrations/*.ts"],
-  // migrationsTableName: "migrations",
-  synchronize: process.env.DB_SYNCHRONIZE === "true",
+  entities: ["apps/**/src/**/entities/*.entity{.ts,.js}"],
+  synchronize: process.env.DB_SYNCHRONIZE === "true" || true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
