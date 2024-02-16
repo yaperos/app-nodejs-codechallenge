db0f16bcce708a058120f686e77da1de863ca763
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-16 17:10:09 -0500

    :sparkles: send message in topic for update transaction

--- apps/consumer/src/operations/operations.service.ts
+++ apps/consumer/src/operations/operations.service.ts
@@ -1,16 +1,27 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import TransactionInDto from "./dto/transaction.in.dto";
+import { ClientProxy } from "@nestjs/microservices";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export default class OperationsService {
+  private readonly topic: string;
+
+  constructor(
+    @Inject("KAFKA_PRODUCER") private readonly kafka: ClientProxy,
+    private readonly configService: ConfigService,
+  ) {
+    this.topic = this.configService.getOrThrow<string>("KAFKA_TOPIC_UPDATED");
+  }
   public processTransactions = async (dto: TransactionInDto) => {
     Logger.log(dto, OperationsService.name);
     const { id, value } = dto;
 
+    let status: number = 1;
     if (value >= 1000) {
       Logger.error("Is fraud");
-    } else {
-      Logger.log("It's ok");
-    }
+      status = 1;
+    } else Logger.log("It's ok");
+    return this.kafka.emit(this.topic, { value: { id, status } });
   };
 }
