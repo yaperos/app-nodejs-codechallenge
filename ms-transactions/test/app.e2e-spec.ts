8e490e4f306c9b168190d135d97dc100a2474ab5
Daniel Mateus <1fvrd7wcms7u360b0kz69fd27zkvp8@bots.bitbucket.org>
2024-03-07 03:33:36 -0500

    test: add health test

--- ms-transactions/test/app.e2e-spec.ts
+++ ms-transactions/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { healthMock } from './mock/health.mock';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -17,8 +18,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect(healthMock);
   });
 });
