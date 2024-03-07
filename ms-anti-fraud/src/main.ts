f805ba5edab4f1b6c3087cfdf6141abcf47f7b27
Daniel Mateus <1fvrd7wcms7u360b0kz69fd27zkvp8@bots.bitbucket.org>
2024-03-07 03:50:12 -0500

    docs: add swagger

--- ms-anti-fraud/src/main.ts
+++ ms-anti-fraud/src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { serverConfig } from './infraestructure/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Yape - MS ANTI FRAUD')
+    .setDescription('Microservice to anti fraud in yape application')
+    .setVersion('1.0')
+    .addTag('ms-anti-fraud')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(serverConfig.port, () => {
     Logger.log(
       `Server is running on port ${serverConfig.port}, ${serverConfig.name}`,
--- ms-transactions/src/application/health/health.controller.ts
+++ ms-transactions/src/application/health/health.controller.ts
@@ -3,7 +3,9 @@ import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
 import { HealthServiceInterface } from 'src/domain/health/health.interface';
 import { HealthCheckResult } from 'src/domain/health/health.model';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Health')
 @Controller('health')
 export class HealthController implements HealthServiceInterface {
   constructor(private readonly healthService: HealthService) {}
