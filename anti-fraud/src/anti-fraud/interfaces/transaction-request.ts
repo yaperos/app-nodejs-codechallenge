d84597e2cff6942c46827b6a759fe959c0b75312
Daniel Rossini <danielrossini7@gmail.com>
2024-03-09 19:17:31 -0300

    feat: transaction interfaces

---
+++ anti-fraud/src/anti-fraud/interfaces/transaction-request.ts
@@ -0,0 +1,4 @@
+export class TransactionReq {
+  transactionExternalId: number;
+  value: number;
+}
---
+++ anti-fraud/src/anti-fraud/interfaces/transaction-response.ts
@@ -0,0 +1,4 @@
+export class TransactionRes {
+  transactionExternalId: number;
+  status: string;
+}
