4d274e95fa3f317ef5f83819f79008e80428ae09
Nancy Clavijo Varela <99740842+Nancyclavijo27@users.noreply.github.com>
2024-03-12 15:07:07 -0500

    posgres

--- microservicio/config/databaseConfig.js
+++ microservicio/config/databaseConfig.js
@@ -1,11 +1,13 @@
 // Carga las variables de entorno desde el archivo .env
 require('dotenv').config();
 
+
 const { Sequelize } = require('sequelize');
 
 // Configura la conexión a la base de datos
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST || 'localhost', // Cambiar a la dirección de tu base de datos si es remota
+  //host: process.env.DB_HOST || 'localhost', // Cambiar a la dirección de tu base de datos si es remota
+  host: process.env.DB_HOST,
   dialect: 'postgres', // El dialecto para PostgreSQL
   port: process.env.DB_PORT || 5432, // El puerto de PostgreSQL por defecto
   // Otros parámetros de configuración si son necesarios
