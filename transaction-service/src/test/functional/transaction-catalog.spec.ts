921e63bf1cdc0a51978fee8e76a96bf6c5d1a681
Michael Salazar <mssalazarb@outlook.com>
2024-03-15 15:33:01 -0500

    [MS-007] Added example testing

---
+++ transaction-service/src/test/functional/transaction-catalog.spec.ts
@@ -0,0 +1,52 @@
+import * as supertest from 'supertest';
+import * as assert from 'assert';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../../app.module';
+
+describe('Transaction Catalog Test', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  describe('Transaction Catalog Test', () => {
+    it('TCT-001 Get transactions catalog by type', async () => {
+      const { statusCode, body } = await supertest(app.getHttpServer())
+        .get('/transaction-catalog')
+        .query({ type: 'TRANSACTION_STATUS' })
+        .send();
+
+      assert.equal(statusCode, 200, 'A correct answer is expected');
+      assert.equal(
+        body[0].name,
+        'PENDING',
+        'Expected receive database register',
+      );
+    });
+
+    it('TCT-002 Get transactions catalog by other type', async () => {
+      const { statusCode, body } = await supertest(app.getHttpServer())
+        .get('/transaction-catalog')
+        .query({ type: 'TRANSACTION_TYPE' })
+        .send();
+
+      assert.equal(statusCode, 200, 'A correct answer is expected');
+      assert.equal(
+        body[0].name,
+        'CREDIT',
+        'Expected receive database register',
+      );
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
